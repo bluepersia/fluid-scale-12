d7e875cecbcd656856203a2e8cbfd8da5c553187
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-10-15 11:07:55 -0600

    Project 1 footer docClone

---
+++ test/golden-master/1/master/parsing/serialization/docClone/footer.ts
@@ -0,0 +1,36 @@
+import {
+  MediaRuleClone,
+  StyleRuleClone,
+  StyleSheetClone,
+} from "../../../../../../../src/parsing/serialization/docSerializer.types";
+
+const docCloneFooter: StyleSheetClone = {
+  cssRules: [
+    {
+      type: 1,
+      selectorText: ".footer",
+      style: {
+        "padding-top": "1rem",
+        "padding-bottom": "1rem",
+      },
+      specialProps: {},
+    } as StyleRuleClone,
+    {
+      type: 4,
+      minWidth: 768,
+      cssRules: [
+        {
+          type: 1,
+          selectorText: ".footer",
+          style: {
+            "padding-top": "2.25rem",
+            "padding-bottom": "2.25rem",
+          },
+          specialProps: {},
+        } as StyleRuleClone,
+      ],
+    } as MediaRuleClone,
+  ],
+};
+
+export { docCloneFooter };
